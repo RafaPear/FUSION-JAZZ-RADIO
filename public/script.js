2894b34c1a5843ed4a3f567c58ca663119ecea5e
RafaPear <rafapear@outlook.pt>
2024-10-06 23:48:30 +0100

    Site set up

---
+++ public/script.js
@@ -0,0 +1,11 @@
+const audioPlayer = document.getElementById('audioPlayer');
+
+// List of songs
+const playlist = [
+    'song1.mp3',
+    'song2.mp3'
+];
+
+let currentTrack = 0;
+
+// Load the first son
