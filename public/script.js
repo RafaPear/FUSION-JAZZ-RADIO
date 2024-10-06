65cf4a8aadc9c443fca0d92dec42c27e1357cbfa
RafaPear <rafapear@outlook.pt>
2024-10-07 00:23:38 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,11 +1,25 @@
 const audioPlayer = document.getElementById('audioPlayer');
-
-// List of songs
-const playlist = [
-    'song1.mp3',
-    'song2.mp3'
-];
-
+let playlist = [];
 let currentTrack = 0;
 
-// Load the first son
+// Fetch the list of songs from the server
+fetch('/songs')
+  .then(response => response.json())
+  .then(songs => {
+    if (songs.length > 0) {
+      playlist = songs;
+      loadTrack(currentTrack);
+    }
+  });
+
+// Load and play the current track
+function loadTrack(index) {
+  audioPlayer.src = `music/${playlist[index]}`;
+  audioPlayer.play();
+}
+
+// Move to the next song when one ends
+audioPlayer.addEventListener('ended', () => {
+  currentTrack = (currentTrack + 1) % playlist.length;
+  loadTrack(currentTrack);
+});
